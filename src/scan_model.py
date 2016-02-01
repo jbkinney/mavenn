ce388d429f367c2214e36fd4b825c7cef488ba84
Justin B. Kinney <jkinney@cshl.edu>
2016-01-31 22:48:51 -0500

    Enabled fasta input to scan_model.
    Will process large fasta contains in chunks of defined size. Also, will
    report only a specified number of sites.

--- src/scan_model.py
+++ src/scan_model.py
@@ -12,6 +12,7 @@ import scipy as sp
 import sys
 #Our miscellaneous functions
 import pandas as pd
+from Bio import SeqIO
 import sst.utils as utils
 import sst.Models as Models
 import sst.io as io
@@ -20,7 +21,7 @@ import re
 import pdb
 from sst import SortSeqError
 
-def main(model_df, contig_dict):
+def main(model_df, contig_list, numsites=10, verbose=False):
 
     # Determine type of string from model
     qc.validate_model(model_df)
@@ -30,7 +31,7 @@ def main(model_df, contig_dict):
     # Check that all characters are from the correct alphabet
     alphabet = qc.seqtype_to_alphabet_dict[seqtype]
     search_string = r"[^%s]"%alphabet
-    for contig_name, contig_str in contig_dict.items():
+    for contig_str, contig_name, pos_offset in contig_list:
         if re.search(search_string,contig_str):
             raise SortSeqError(\
                 'Invalid character for seqtype %s found in %s.'%\
@@ -45,39 +46,50 @@ def main(model_df, contig_dict):
     # Create list of dataframes, one for each contig
     seq_col = qc.seqtype_to_seqcolname_dict[seqtype]
     L = model_obj.length
-    these_dfs = []
-    for contig_name, contig_str in contig_dict.items():
+    sitelist_df = pd.DataFrame(\
+            columns=['val',seq_col,'left','right','ori','contig'])
+    for contig_str, contig_name, pos_offset in contig_list:
         if len(contig_str) < L:
             continue
         this_df = pd.DataFrame(\
             columns=['val',seq_col,'left','right','ori','contig'])
         num_sites = len(contig_str) - L + 1
-        lefts = np.arange(num_sites).astype(int)
-        this_df['left'] = lefts
-        this_df['right']  = lefts + L - 1
-        this_df[seq_col] = [contig_str[i:(i+L)] for i in lefts]
+        poss = np.arange(num_sites).astype(int) 
+        this_df['left'] = poss + pos_offset
+        this_df['right']  = poss + pos_offset + L - 1 
+        this_df[seq_col] = [contig_str[i:(i+L)] for i in poss]
         this_df['ori'] = '+'
         this_df['contig'] = contig_name
         this_df['val'] = model_obj.evaluate(this_df[seq_col])
-        these_dfs.append(this_df.copy())
+        sitelist_df = pd.concat([sitelist_df,this_df], ignore_index=True)
 
         # If scanning DNA, scan reverse-complement as well
         if seqtype=='dna':
             this_df[seq_col] = [qc.rc(s) for s in this_df[seq_col]]
             this_df['ori'] = '-'
             this_df['val'] = model_obj.evaluate(this_df[seq_col])
-            these_dfs.append(this_df.copy())
+            sitelist_df = pd.concat([sitelist_df,this_df], ignore_index=True)
+
+        # Sort by value and reindex
+        sitelist_df.sort(columns='val', ascending=False, inplace=True)
+        sitelist_df.reset_index(drop=True,inplace=True)
+
+        # Crop list at numsites
+        if sitelist_df.shape[0]>numsites:
+            sitelist_df.drop(sitelist_df.index[numsites:], inplace=True)
+
+        if verbose:
+            print '.',
+            sys.stdout.flush()
+
+    if verbose:
+        print ''
+        sys.stdout.flush()
 
     # If no sites were found, raise error
-    if len(these_dfs)==0:
+    if sitelist_df.shape[0]==0:
         raise SortSeqError(\
             'No full-length sites found within provided contigs.')
-
-    # Concatenate dataframes
-    sitelist_df = pd.concat(these_dfs,ignore_index=True)
-
-    # Sort by value and reindex
-    sitelist_df.sort(columns='val', ascending=False, inplace=True)
 
     sitelist_df = qc.validate_sitelist(sitelist_df,fix=True)
     return sitelist_df
@@ -85,19 +97,59 @@ def main(model_df, contig_dict):
 
 def wrapper(args):
     """ Wrapper for function for scan_model.main()
-    """  
+    """
+
     # Prepare input to main
     model_df = io.load_model(args.model)
+    seqtype, modeltype = qc.get_model_type(model_df)
+    L = model_df.shape[0]
+    if modeltype=='NBR':
+        L += 1 
 
-    # Create contig_dict from sequences
+    # Do not currently use pos offset
+    
+    chunksize = args.chunksize
+
+    # If sequence is provided manually
     if args.seq:
-        contig_dict = {'manual':args.seq}
+        pos_offset=0
+        contig_str = args.seq
+
+        # Add a bit on end if circular
+        if args.circular:
+            contig_str += contig_str[:L-1] 
+
+        contig_list = [(contig_str,'manual',pos_offset)]
+
+    # Otherwise, read sequence from FASTA file
     else:
-        raise SortSeqError('No input sequences provided.')
+        contig_list = []
+        inloc = io.validate_file_for_reading(args.i) if args.i else sys.stdin
+        for i,record in enumerate(SeqIO.parse(inloc,'fasta')):
+            name = record.name if record.name else 'contig_%d'%i
+
+             # Split contig up into chunk)size bits
+            full_contig_str = str(record.seq)
+
+            # Add a bit on end if circular
+            if args.circular:
+                full_contig_str += full_contig_str[:L-1] 
+
+            pos_offset = 0
+            while pos_offset+chunksize < len(full_contig_str):
+                contig_str = full_contig_str[pos_offset:pos_offset+chunksize]
+                contig_list.append((contig_str,name,pos_offset))
+                pos_offset += chunksize-L
+            contig_str = full_contig_str[pos_offset:]
+            contig_list.append((contig_str,name,pos_offset))
+
+        if len(contig_list)==0:
+            raise SortSeqError('No input sequences to read.')
 
     # Compute results
     outloc = io.validate_file_for_writing(args.out) if args.out else sys.stdout
-    output_df = main(model_df,contig_dict)
+    output_df = main(model_df,contig_list,numsites=args.numsites,\
+        verbose=args.verbose)
 
     # Write df to stdout or to outfile 
     io.write(output_df,outloc,fast=args.fast)
@@ -107,12 +159,31 @@ def add_subparser(subparsers):
     p = subparsers.add_parser('scan_model')
     p.add_argument(
         '-s','--seq',type=str,default=None,
-        help ='sequence to scan')
+        help ='manually enter sequence to scan')
+    p.add_argument(
+        '-i','--i',type=str,default=None,
+        help ='specify FASTA sequence contig file')
     p.add_argument(
         '-m','--model', help='model to scan sequence with')
     p.add_argument('-o', '--out', default=None)
     p.add_argument(
         '-f','--fast', action='store_true', 
         help="Output is a little harder to read, but is written much faster."
+        )
+    p.add_argument(
+        '-n','--numsites', type=int, default=10, 
+        help="Maximum number of sites to record."
+        )
+    p.add_argument(
+        '-k','--chunksize', type=int, default=10000, 
+        help="chunksize to use when parsing FASTA files."
+        )
+    p.add_argument(
+        '-c','--circular', action='store_true', 
+        help="Treat sequences as circular."
+        )
+    p.add_argument(
+        '-v','--verbose', action='store_true', 
+        help="provides updates on scan. "
         )
     p.set_defaults(func=wrapper)
