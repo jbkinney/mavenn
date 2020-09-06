1e60a324a66cdfd416947be2f24665245622065a
Justin B. Kinney <jkinney@cshl.edu>
2020-09-06 18:19:14 -0400

    Minor modifications to examples.py

--- mavenn/src/examples.py
+++ mavenn/src/examples.py
@@ -10,7 +10,47 @@ from mavenn.src.error_handling import handle_errors, check
 
 
 @handle_errors
-def run_demo(name=None, print_code=True):
+def list_tutorials():
+    """
+    Reveals the local directory in which mavenn tutorials are stored.
+    """
+    tutorials_dir = mavenn.__path__[0] + '/examples/tutorials'
+    tutorial_file_names = glob.glob(f'{tutorials_dir}/*.ipynb')
+    tutorial_base_names = [file_name.split('/')[-1]
+                            for file_name in tutorial_file_names]
+    tutorial_base_names.sort()
+
+    print(f"The following MAVE-NN tutorials are available (as Jupyter notebooks):\n")
+    for name in tutorial_base_names:
+        print(f'\t{name}')
+    print(f"\nThese tutorial files are located in\n\n\t{tutorials_dir}/\n")
+
+
+@handle_errors
+def list_demos():
+    """
+    Reveals the local directory in which mavenn demos are stored.
+    """
+    demos_dir = mavenn.__path__[0] +'/examples/demos'
+    demo_file_names = glob.glob(f'{demos_dir}/*.py')
+    demos_dict = {}
+    for file_name in demo_file_names:
+        base_name = file_name.split('/')[-1]
+        pattern = '^(.*)\.py$'
+        key = re.match(pattern, base_name).group(1)
+        demos_dict[key] = file_name
+    demo_names = list(demos_dict.keys())
+    demo_names.sort()
+
+    print("To run a demo, execute\n\n\t>>> mavenn.run_demo(name)"
+          "\n\nwhere 'name' is one of the following strings:\n")
+    for i, name in enumerate(demo_names):
+        print(f'\t{i+1}. "{name}"')
+    print(f"\nPython code for each demo is located in\n\n\t{demos_dir}/\n")
+
+
+@handle_errors
+def run_demo(name=None, print_code=False):
     """
     Performs a demonstration of the mavenn software.
 
@@ -61,10 +101,12 @@ def run_demo(name=None, print_code=True):
                 print('Running %s:\n%s\n%s\n%s' % \
                       (file_name, line, content, line))
         else:
-            pass
+            print(f'Running {file_name}...')
 
         # Run run_demo
         exec(open(file_name).read())
+
+        print('Done!')
 
     # If input is invalid, raise error
     else:
