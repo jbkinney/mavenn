0321810a0104a9dc68d7fd73cf0cce39c9065d64
mahdikooshkbaghi <mahdi.kooshkbaghi@gmail.com>
2021-12-13 14:35:12 -0500

    add the script for produce the tdp43 and abeta models in the paper

--- paper/training_scripts/additive_models_training.py
+++ paper/training_scripts/additive_models_training.py
@@ -1,6 +1,5 @@
 """
-This script is for training the ABeta and TDP-43 models 
-for the paper
+This script is for training models for the paper
 MAVE-NN: learning genotype-phenotype maps from multiplex assays of variant effect
 Ammar Tareen, Mahdi Kooshkbaghi, Anna Posfai, 
 William T. Ireland, David M. McCandlish, Justin B. Kinney
@@ -10,14 +9,22 @@ import numpy as np
 import argparse
 import pandas as pd
 import random
+import warnings
 import os
-from datetime import date
-# Fix all possible seeds
+from datetime import datetime
+import json
+# Turn off Tensorflow GPU warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+warnings.filterwarnings('ignore')
+
+# Fix all the possible seeds
 import tensorflow as tf
-tf.random.set_seed(1234)
-np.random.seed(1234)
-random.seed(1234)
-os.environ["PYTHONHASHSEED"] = str(1234)
+
+seed = 1234
+tf.random.set_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
+os.environ["PYTHONHASHSEED"] = str(seed)
 
 
 # Import MAVE-NN local
@@ -27,33 +34,13 @@ sys.path.insert(0, "/home/mahdik/workspace/mavenn")
 import mavenn
 
 # Get the date to append to the saved model name
-today = date.today()
-date_str = today.strftime("%d_%m_%Y")
+today = datetime.now()
+date_str = today.strftime("%d_%m_%Y_%H_%M")
 
+# Get the models dictionary from the json input file.
+input_file = open("additive_models_param.json")
+models_dict = json.load(input_file)
 
-# This dict defines the dataset name and
-# the associated parameters used for training the models
-# in the paper.
-models_dict = {}
-
-# Parameters for the amyloid beta trained in the paper
-models_dict["amyloid"] = {
-    "dataset_name":"amyloid",
-    "model_params":{
-        "alphabet": "protein*",
-        "gpmap_type": "additive",
-        "regression_type": "GE",
-        "ge_noise_model_type": "SkewedT",
-        "ge_heteroskedasticity_order": 2
-    },
-    "fit_params":{
-        "learning_rate": 1e-3,
-        "epochs": 500,
-        "batch_size": 64,
-        "early_stopping_patience": 25,
-        "early_stopping":True
-    }
-}
 
 def main(args):
 
@@ -64,7 +51,7 @@ def main(args):
     params_dict = models_dict[dataset_name]
 
     # TODO: Change to pandas
-    data_df = mavenn.load_example_dataset(dataset_name)
+    data_df = pd.read_csv(f'../datasets/{dataset_name}_data.csv.gz')
 
     # Get and report sequence length
     L = len(data_df.loc[0, "x"])
@@ -80,10 +67,7 @@ def main(args):
     print("trainval_df:")
 
     # Define model
-    model = mavenn.Model(
-        L=L,
-        **params_dict["model_params"]
-    )
+    model = mavenn.Model(L=L, **params_dict["model_params"])
 
     # Set training data
     model.set_data(
@@ -93,10 +77,7 @@ def main(args):
     )
 
     # Train model
-    model.fit(
-        verbose=False,
-        **params_dict["fit_params"]
-    )
+    model.fit(verbose=False, **params_dict["fit_params"])
 
     # Compute variational information on test data
     I_var, dI_var = model.I_variational(x=test_df["x"], y=test_df["y"])
@@ -107,7 +88,7 @@ def main(args):
     print(f"test_I_pred: {I_pred:.3f} +- {dI_pred:.3f} bits")
 
     # Save model to file
-    model_name = f"{dataset_name}_additive_ge_{date_str}"
+    model_name = f"../models/{dataset_name}_additive_ge_{date_str}"
     model.save(model_name)
 
 
