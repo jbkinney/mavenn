dc6beb6c9ffd40e5d02b47322fc37613d89912ed
mahdikooshkbaghi <mahdi.kooshkbaghi@gmail.com>
2021-12-22 11:54:08 -0500

    modify the xylE to run on Elzar

--- paper/training_scripts/xylE_thermo_training.py
+++ paper/training_scripts/xylE_thermo_training.py
@@ -6,6 +6,7 @@ William T. Ireland, David M. McCandlish, Justin B. Kinney
 """
 
 # Standard imports
+import sys
 import numpy as np
 import pandas as pd
 import warnings
@@ -21,18 +22,15 @@ warnings.filterwarnings("ignore")
 
 # Standard TensorFlow imports
 import tensorflow as tf
-import tensorflow.keras.backend as K
 from tensorflow.keras.initializers import Constant
+import tensorflow.keras.backend as K
 
 # Import MAVE-NN local
-import sys
-
 sys.path.insert(0, "../../")
 import mavenn
-
-# Import base class
-from mavenn.src.layers.gpmap import GPMapLayer
 from mavenn.src.utils import _x_to_mat
+from mavenn.src.layers.gpmap import GPMapLayer
+
 
 # Fix the seed, default seed is seed=1234
 mavenn.src.utils.set_seed()
@@ -122,7 +120,6 @@ class xylEGPMapLayer(GPMapLayer):
         # 4 Define theta_CRP_lc parameters
         theta_CRP_lc_shape = (1, self.L_CRP, self.C)
 
-        theta_CRP_lc_init = np.random.randn(*theta_CRP_lc_shape) / np.sqrt(self.L_CRP)
         self.theta_CRP_lc = self.add_weight(
             name="theta_CRP_lc",
             shape=theta_CRP_lc_shape,
@@ -133,9 +130,6 @@ class xylEGPMapLayer(GPMapLayer):
         # 5 Define theta_xylR_lc parameters
         theta_xylR_lc_shape = (1, self.L_xylR, self.C)
 
-        theta_xylR_lc_init = np.random.randn(*theta_xylR_lc_shape) / np.sqrt(
-            self.L_xylR
-        )
         self.theta_xylR_lc = self.add_weight(
             name="theta_xylR_lc",
             shape=theta_xylR_lc_shape,
@@ -171,9 +165,7 @@ class xylEGPMapLayer(GPMapLayer):
         )
 
         # 10 define tsat term.
-        self.tsat = self.add_weight(
-            name="tsat", shape=(1,), initializer=Constant(1.0), trainable=True
-        )
+        self.tsat = 1.0
 
         # Call superclass build
         super().build(input_shape)
@@ -187,9 +179,9 @@ class xylEGPMapLayer(GPMapLayer):
         """
 
         # extract locations of binding sites from entire lac-promoter sequence.
-        x_CRP_lc = x_lc[:, self.C * self.CRP_start : self.C * self.CRP_end]
-        x_xylR_lc = x_lc[:, self.C * self.xylR_start : self.C * self.xylR_end]
-        x_RNAP_lc = x_lc[:, self.C * self.RNAP_start : self.C * self.RNAP_end]
+        x_CRP_lc = x_lc[:, self.C * self.CRP_start: self.C * self.CRP_end]
+        x_xylR_lc = x_lc[:, self.C * self.xylR_start: self.C * self.xylR_end]
+        x_RNAP_lc = x_lc[:, self.C * self.RNAP_start: self.C * self.RNAP_end]
 
         # reshape according to crp and rnap lengths.
         x_CRP_lc = tf.reshape(x_CRP_lc, [-1, self.L_CRP, self.C])
@@ -239,17 +231,17 @@ class xylEGPMapLayer(GPMapLayer):
 
 def main(args):
 
-    # Set dataset name
-    dataset_name = "xylE"
-
     # Set type of GP map
     model_type = "custom"
 
     # Set learning rate
-    learning_rate = args.learning_rate
+    lr = args.learning_rate
 
     # Set number of epochs
-    epochs = args.epochs
+    ep = args.epochs
+
+    # Set batch size
+    bs = args.batch_size
 
     # Get parameters dict
     params_dict = models_dict[model_type]
@@ -272,6 +264,14 @@ def main(args):
     # Preview trainval_df
     print("trainval_df:")
 
+    # modify the params_dict gpmap_kwargs
+    re = args.regularizer
+    params_dict["gpmap_kwargs"]["regularizer"] = re
+    params_dict["gpmap_kwargs"]["theta_regularization"] = re
+
+    # Just print to check elzar specified the var correctly
+    print(f'lr: {lr}, bs:{bs}, re:{re}')
+
     # Define model
 
     # Create model instance
@@ -289,20 +289,32 @@ def main(args):
     )
 
     # Train model
-    model.fit(learning_rate=learning_rate, epochs=epochs, **params_dict["fit_params"])
+    model.fit(learning_rate=lr, epochs=ep,
+              batch_size=bs,
+              **params_dict["fit_params"])
 
     # Save model to file
-    model_name = f"../models/xylE_{model_type}_lr_{learning_rate}_e_{epochs}_{date_str}"
+    model_name = f"../models/xylE_{model_type}_lr_{lr}_bs_{bs}_re_{re}_{date_str}"
+    print(f'model is saved in {model_name}')
     model.save(model_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="xylE Thermodynamic Model")
     parser.add_argument(
-        "-e", "--epochs", default=200, type=int, help="Number of epochs"
+        "-ep", "--epochs", default=500, type=int, help="Number of epochs"
     )
     parser.add_argument(
-        "-lr", "--learning_rate", default=3e-4, type=float, help="Number of epochs"
+        "-lr", "--learning_rate", default=5e-4, type=float, help="Number of epochs"
     )
+
+    parser.add_argument(
+        "-bs", "--batch_size", default=1000, type=int, help="Batch size"
+    )
+
+    parser.add_argument(
+        "-re", "--regularizer", default=1e-1, type=float, help="Regularization in gpmap"
+    )
+
     args = parser.parse_args()
     main(args)
